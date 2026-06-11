007e8ae65f217d8a8cd537a1ceed178afd362c5b
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-06-10 21:59:37 -0300

    Initial Commit

---
+++ SwiftExpenseTrackerApp/SwiftExpenseTrackerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftExpenseTrackerAppApp.swift
+//  SwiftExpenseTrackerApp
+//
+//  Created by Claudio da Silva Arauo Filho on 10/06/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftExpenseTrackerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
