4eb00a45184542d34d81d29dae3192a517d09cb8
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-06-11 07:45:29 -0300

    initial commit

---
+++ SwiftExpenseTrackerApp/Features/Home/Views/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  SwiftExpenseTrackerApp
+//
+//  Created by Claudio da Silva Arauo Filho on 11/06/26.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        LazyVStack{
+            
+        }
+    }
+}
+
+#Preview {
+    HomeView()
+}
