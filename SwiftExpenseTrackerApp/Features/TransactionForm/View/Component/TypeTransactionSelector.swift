f311736f0cce9984e98f3f4da5b37b3014698adf
Claudio712005 <clausilvaaraujo11@gmail.com>
2026-06-15 18:46:02 -0300

    feature: Add Transaction Selector

---
+++ SwiftExpenseTrackerApp/Features/TransactionForm/View/Component/TypeTransactionSelector.swift
@@ -0,0 +1,18 @@
+//
+//  TypeTransactionSelector.swift
+//  SwiftExpenseTrackerApp
+//
+//  Created by Claudio da Silva Arauo Filho on 15/06/26.
+//
+
+import SwiftUI
+
+struct TypeTransactionSelector: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TypeTransactionSelector()
+}
